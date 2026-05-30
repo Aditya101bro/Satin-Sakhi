eafc5e5a8a0dc5fda140681d8aa8281d3eaa6842
Aditya101bro <78101265+Aditya101bro@users.noreply.github.com>
2026-05-30 19:11:53 +0530

    Update face_capture_screen.dart

--- lib/screens/face_capture_screen.dart
+++ lib/screens/face_capture_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
